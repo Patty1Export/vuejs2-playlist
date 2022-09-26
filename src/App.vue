03a62a769f256dacdc932493499f5ae00b61c50c
Patty <114051644+Patty1Export@users.noreply.github.com>
2022-09-26 16:54:27 +0800

    Update title slot

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,29 @@
 <template>
   <div>
-
-    <!--rendering updated titles-->
-    <app-header v-bind:title="title" v-on:changeTitle="updateTitle($event)"></app-header>
-    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas> <!--add props for the template-->
-    <app-footer v-bind:title="title"></app-footer>
+    <form-helper>
+      <h2 slot="title">{{title}}</h2>
+      <p slot="text">I am the the paragraph text of the slot</p> <!--Add slot attribute with a "text" name-->
+    </form-helper>
   </div>
 
 </template>
 <!--Update Both Components in Header and Footer-->
 <script>
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
-import Ninjas from './components/Ninjas.vue';
+import formHelper from './components/formHelper.vue';
 
 export default{
   components:{
-    'app-header':Header,
-    'app-footer':Footer,
-    'app-ninjas':Ninjas,
+    'form-helper':formHelper,
   },
   data(){
     return{
-      ninjas:[
-        {name:'Ryu', specialty:'Vue Components', show:false},
-        {name:'Crystal', specialty:'HTML Wizardy', show:false},
-        {name:'Histoshi', specialty:'Click Events', show:false},
-        {name:'Tango', specialty:'Conditionals', show:false},
-        {name:'Kami', specialty:'Webpack', show:false},
-        {name:'Yoshi', specialty:'Data Diggin', show:false},
-      ],
-      title:"Vue Ninjas"
+      'title':'I am a dynamic slot title'
     }
   },
 
   //reacted events
   methods:{
-    updateTitle: function(updatedTitle){
-      this.title = updatedTitle;
-    }
+
   }
  }
 
